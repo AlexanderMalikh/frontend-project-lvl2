2775fd6e4c2f4c2dd903c0510d832b26a8f34194
Alexander Malykh <fulgrimka1@gmail.com>
2019-10-13 15:17:04 +0300

    travis

--- __tests__/test.plain json.js
+++ __tests__/test.plain json.js
@@ -3,7 +3,10 @@ import genDiff from '../src/parsers/plain json';
 
 const fs = require('fs');
 
+const plainJsonBefore = `${__dirname}/__fixtures__/before.json`;
+const plainJsonAfter = `${__dirname}/__fixtures__/after.json`;
+const plainJsonResult = fs.readFileSync(`${__dirname}/__fixtures__/plainJsonResult`, 'utf-8');
+
 test('two plain json\'s', () => {
-  expect(genDiff(`${__dirname}/__fixtures__/before.json`, `${__dirname}/__fixtures__/after.json`))
-    .toBe(_.trimEnd(fs.readFileSync(`${__dirname}/__fixtures__/plainJsonResult`, 'utf-8')));
+  expect(genDiff(plainJsonBefore, plainJsonAfter)).toBe(_.trimEnd(plainJsonResult));
 });

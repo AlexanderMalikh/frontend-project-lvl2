49ff968bc920ea1d9e2c6e74e3ab27c235f73b51
Alexander <fulgrimka1@gmail.com>
2019-12-19 19:37:56 +0300

    treeRender done

--- src/formatters/treeRender.js
+++ src/formatters/treeRender.js
@@ -1,29 +1,31 @@
 import _ from 'lodash';
-
-const makeIndent = (deep) => '  '.repeat(deep);
+// отступ для } = накопленный отступ - внешний отступ
+const indentLevel = 1; // внутренний отступ
+const indentLength = 2; // внешний отступ
+const makeIndent = (deep, level = 0) => '  '.repeat(deep - level);
 
 const stringify = (node, deep = 0) => {
   if (_.isObject(node)) {
-    const content = _.keys(node).map((elem) => `{\n${makeIndent(deep + 2)}${elem}: ${node.key}\n${makeIndent(deep)}}`).join('\n');
-    return `${content}`;
+    const content = _.keys(node).map((elem) => `${makeIndent(deep + 1, indentLevel)}${elem}: ${stringify(node[elem], deep + indentLength)}`).join('\n');
+    return `{\n${content}\n${makeIndent(deep, indentLength)}}`;
   }
   return node;
 };
 
 const mapByStatus = {
-  tree: (item, deep, func) => `${makeIndent(deep)}${item.key}: ${(func(item.children, deep + 2))}`,
+  tree: (item, deep, func) => `${makeIndent(deep)}${item.key}: ${(func(item.children, deep + indentLength))}`,
   unchanged: (item, deep) => `${makeIndent(deep)}${item.key}: ${stringify(item.beforeValue, deep)}`,
-  changed: (item, deep) => [`${makeIndent(deep - 1)}+ ${item.key}: ${stringify(item.afterValue, deep)},
-    ${makeIndent(deep - 1)}- ${item.key}: ${stringify(item.beforeValue, deep)}`],
-  added: (item, deep) => `${makeIndent(deep - 1)}+ ${item.key}: ${stringify(item.afterValue, deep)}`,
-  removed: (item, deep) => `${makeIndent(deep - 1)}- ${item.key}: ${stringify(item.beforeValue, deep)}`,
+  changed: (item, deep) => [`${makeIndent(deep, indentLevel)}+ ${item.key}: ${stringify(item.afterValue, deep + indentLength)}`,
+    `${makeIndent(deep, indentLevel)}- ${item.key}: ${stringify(item.beforeValue, deep + indentLength)}`],
+  added: (item, deep) => `${makeIndent(deep, indentLevel)}+ ${item.key}: ${stringify(item.afterValue, deep + indentLength)}`,
+  removed: (item, deep) => `${makeIndent(deep, indentLevel)}- ${item.key}: ${stringify(item.beforeValue, deep + indentLength)}`,
 };
 
-const treeRender = (ast, deep = 2) => {
-  console.log(ast);
+const treeRender = (ast, deep = indentLength) => {
+  // console.log(ast);
   const getStrings = ast.map((node) => mapByStatus[node.status](node, deep, treeRender));
   const renderedAst = _.flatten(getStrings).join('\n');
-  return `{\n${renderedAst}\n}`;
+  return `{\n${renderedAst}\n${makeIndent(deep, indentLength)}}`;
 };
 
 export default treeRender;
